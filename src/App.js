aab0221f6aa3bb788b9ce95d266e16ddee2ccc69
yasuko <y.otsubo04@gmail.com>
2024-12-06 11:03:24 +0900

    課題1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
+
+export const App = () => {
+   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          課題1
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
+ 
---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import { posts } from '../data/posts';
+import { Link } from 'react-router-dom';
+
+
+const Home = () =>{
+  return (
+    <div className='my-10'>
+      {posts.map((post) => (
+        <article className=" w-1/2 mx-auto py-3 px-2 mb-5 border-gray-200 border-2	" key={post.id}>
+                      <Link to='/'>
+
+          <div className=' px-1 py-1 w-full flex justify-between'>
+            <time className=' text-gray-400'>{new Date(post.createdAt).toLocaleDateString()}</time>
+            <div className='flex gap-1'>
+              {post.categories.map((category) => (
+              <div className='border p-2 border-blue-400 rounded-lg	 text-blue-400	text-xs' key={category}>
+                {category}
+                </div>
+              ))}
+            </div>
+          </div>
+          <h2 className='text-xl	mb-5	'>{post.title}</h2>
+          <p className="text-sm h-10 line-clamp-2" dangerouslySetInnerHTML={{ __html: post.content }}></p>
+          </Link>
+
+          </article>
+
+      ))}
+    </div>
+  )  
+}
+export default Home; 
+
